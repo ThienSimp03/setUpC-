50aacd70ff1eaa3ed16a2bcfe8de3bd1d7181a22
Thien Simp 03 <104013686+ThienSimp03@users.noreply.github.com>
2024-09-08 21:55:22 +0700

    add debug

--- setup.cpp
+++ setup.cpp
@@ -13,14 +13,24 @@ using namespace std;
 #define tiii tuple<int, int, int>
 #define vi vector<int>
 #define vl vc<ll>
-#define vll vector<pll>
 #define vii vector<pii>
+#define vll vector<pll>
 #define vb vector<bool>
 #define vc vector
-#define L cout << '\n';
+#define mpi map<int, int>
+#define mpl mp<ll, ll>
+#define ump unordered_map<int, int>
+#define umpl unordered_map<ll, l>
+#define si set<int>
+#define sl set<ll>
+#define usi unordered_set<int>
+#define usl unordered_set<ll>
+#define el cout << '\n';
 #define all(x) x.begin(), x.end()
 #define rep(i, a, b) for (int i = a; i < b; ++i)
-#define rev(i, a, b) for (int i = a; i > b; --i)
+#define f0(i, n) for (int i = 0; i < n; ++i)
+#define f1(i, n) for (int i = 1; i <= n; ++i)
+#define rev(i, a, b) for (int i = a; i >= b; --i)
 #define IOS                           \
     ios_base::sync_with_stdio(false); \
     cin.tie(nullptr);
@@ -48,8 +58,79 @@ using namespace std;
 #define ms0(a) memset(a, 0, sizeof(a))
 #define yes() cout << "YES\n"
 #define no() cout << "NO\n"
+#define debug(x)       \
+    cerr << #x << " "; \
+    _print(x);         \
+    cerr << endl;
 
-#define mo 1000000007
+void _print(ll t) { cerr << t; }
+void _print(int t) { cerr << t; }
+void _print(string t) { cerr << t; }
+void _print(char t) { cerr << t; }
+void _print(double t) { cerr << t; }
+void _print(float t) { cerr << t; }
+void _print(ull t) { cerr << t; }
+void _print(bool x)
+{
+    if (x)
+    {
+        cerr << "True";
+    }
+    else
+        cerr << "False";
+}
+template <class T, class S>
+void _print(pair<T, S> p)
+{
+    cerr << p.first << " " << p.second << endl;
+}
+
+template <class T>
+void _print(vector<T> v)
+{
+    cerr << "[ ";
+    for (T i : v)
+    {
+        _print(i);
+        cerr << " ";
+    }
+    cerr << "]";
+}
+
+template <class T>
+void _print(set<T> v)
+{
+    cerr << "[ ";
+    for (T i : v)
+    {
+        _print(i);
+        cerr << " ";
+    }
+    cerr << "]";
+}
+template <class T>
+void _print(multiset<T> v)
+{
+    cerr << "[ ";
+    for (T i : v)
+    {
+        _print(i);
+        cerr << " ";
+    }
+    cerr << "]";
+}
+
+template <class T, class S>
+void _print(map<T, S> v)
+{
+    cerr << "[ ";
+    for (auto i : v)
+    {
+        _print(i);
+        cerr << endl;
+    }
+    cerr << " ]";
+}
 
 template <class T, auto S>
 std::ostream &operator<<(std::ostream &os, const std::array<T, S> &v)
@@ -58,6 +139,7 @@ std::ostream &operator<<(std::ostream &os, const std::array<T, S> &v)
         os << i << ' ';
     return os;
 }
+
 template <class T, auto S, auto V>
 std::ostream &operator<<(std::ostream &os, const std::array<std::array<T, S>, V> &v)
 {
@@ -67,6 +149,7 @@ std::ostream &operator<<(std::ostream &os, const std::array<std::array<T, S>, V>
     os << "\n";
     return os;
 }
+
 template <class T>
 std::ostream &operator<<(std::ostream &os, const std::vector<T> &v)
 {
@@ -74,6 +157,7 @@ std::ostream &operator<<(std::ostream &os, const std::vector<T> &v)
         os << i << ' ';
     return os;
 }
+
 template <class T>
 std::ostream &operator<<(std::ostream &os, const std::set<T> &v)
 {
@@ -81,6 +165,7 @@ std::ostream &operator<<(std::ostream &os, const std::set<T> &v)
         os << i << ' ';
     return os;
 }
+
 template <class T, class S>
 std::ostream &operator<<(std::ostream &os, const std::map<T, S> &v)
 {
@@ -88,12 +173,14 @@ std::ostream &operator<<(std::ostream &os, const std::map<T, S> &v)
         os << i.first << " " << i.second << "\n";
     return os;
 }
+
 template <class T, class S>
 std::ostream &operator<<(std::ostream &os, const std::pair<T, S> &v)
 {
     os << v.first << " " << v.second << "\n";
     return os;
 }
+
 template <class T>
 std::ostream &operator<<(std::ostream &os, const std::vector<std::vector<T>> &v)
 {
@@ -103,9 +190,13 @@ std::ostream &operator<<(std::ostream &os, const std::vector<std::vector<T>> &v)
     os << "\n";
     return os;
 }
+#define mo 1000000007
 
-void solve()
+int n, ans, q;
+
+void solve(int tc)
 {
+    ans = 0;
 }
 
 int main()
@@ -113,9 +204,9 @@ int main()
     IOS;
     int TC;
     cin >> TC;
-    while (TC--)
+    f1(tc, TC)
     {
-        solve();
+        solve(tc);
     }
     return 0;
 }
